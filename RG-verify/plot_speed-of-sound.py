Fixed some speed of sound Jacobian calculation errors and updated script for plotting the results

RG-verify/plot_speed-of-sound.py
@@ -22,10 +22,12 @@ if __name__ == "__main__":
 
     c_table = []
 
+    T_skip = 24
+
     for p_idx in range(n_p):
         c_list = []
 
-        for T_idx in range(1, n_T):
+        for T_idx in range(T_skip, n_T):
             data_info = data_RG8[p_idx, T_idx]
 
             p = data_info[0]
@@ -37,6 +39,8 @@ if __name__ == "__main__":
 
             a = model.compute_a()
             c = a**2*rho/p
+
+            print("p: %.3e, T: %.3e, c:%.3e"%(p, T, c))
 
             c_list += [c]
         c_table += [c_list]
@@ -54,11 +58,33 @@ if __name__ == "__main__":
     fig, ax = plt.subplots(figsize=(8, 5)) 
     colors = plt.cm.jet(np.linspace(0, 1, n_p))
 
-
-    T_array = data_RG8[0, :, 1][1:]
+    T_array = data_RG8[0, :, 1][T_skip:]
     for p_idx in range(n_p):
         c_array = c_table[p_idx]
         ax.plot(T_array, c_array, "-", color=colors[p_idx])
+
+    lines = ax.get_lines()
+    # Legend (color)
+    label1 = ["0.0001", "0.001", "0.01", "0.1", "1", "10", "100"]
+    legend1 = plt.legend([lines[i] for i in range(n_p)], label1,
+        title="Pressure (atm)", title_fontsize=12,
+        fontsize=10, loc="lower center", ncol=len(label1))
+    ax.add_artist(legend1)
+
+    xlim = [0, 16000]
+    ylim = [0.9, 1.7]
+
+    ax.set_xticks(np.linspace(xlim[0], xlim[1], 9))
+    ax.set_xticks(np.linspace(xlim[0], xlim[1], 33), minor=True)
+    ax.set_yticks(np.linspace(ylim[0], ylim[1], 9))
+    ax.set_yticks(np.linspace(ylim[0], ylim[1], 17), minor=True)
+
+    plt.ylabel(r"Speed of Sound ($\frac{a^2 \rho}{p}$)", fontsize=14)
+    plt.xlabel(r"Temperature ($T$)", fontsize=14)
+
+    plt.ylim(ylim)
+    plt.xlim([0, 15000])
+    plt.grid(which="both")
 
     plt.show()
 

RG-verify/verify-RG8-Jacobian-a.py
@@ -7,12 +7,51 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append("../libs/real-gas/")
 from Species import Monatomic, Diatomic
-from Mixture import Reaction, Mixture, res_RG8, res_RG8_prime 
+from Mixture import Reaction, Mixture, get_RG8_a_jacobian
 from gas_models import get_model_RG8
 
 
-def res_a():
-    pass
+def res_a(U, mixture):
+    eta_all = U[:8]
+    rho = U[-2]
+    T = U[-1]
+
+    K_p_all = mixture.compute_K_p(T)
+    ratio_NO = mixture.ratio_NO
+    R_hat = mixture.R_hat
+    n_s = mixture.n_species
+
+    rhoRT = rho*R_hat*T
+
+    res = np.zeros(10)
+
+    res[0] =   eta_all[3]**2/eta_all[0] - K_p_all[0]/rhoRT
+    res[1] =   eta_all[4]**2/eta_all[1] - K_p_all[1]/rhoRT
+    res[2] = eta_all[3]*eta_all[4]/eta_all[2] - K_p_all[2]/rhoRT
+    res[3] = eta_all[5]*eta_all[7]/eta_all[3] - K_p_all[3]/rhoRT
+    res[4] = eta_all[6]*eta_all[7]/eta_all[4] - K_p_all[4]/rhoRT
+
+    res[5] = (2*eta_all[0] + eta_all[2] + eta_all[3] + eta_all[5])/\
+        (2*eta_all[1] + eta_all[2] + eta_all[4] + eta_all[6]) - ratio_NO
+
+    res[6] = eta_all[5] + eta_all[6] - eta_all[7]
+
+
+    mixture.set_T(T)
+
+    for s_idx in range(n_s):
+        species = mixture.species_list[s_idx]
+        p_s = eta_all[s_idx]*rho*R_hat*T
+        species.compute_entropy(p_s)
+        s_total = species.s_total
+
+        cs = eta_all[s_idx]*species.M_hat*s_total
+
+        res[7] += species.M_hat*eta_all[s_idx]
+        res[8] += p_s
+        res[9] += cs
+
+    return res
 
 if __name__ == "__main__":
     # initialize gas model
@@ -25,8 +64,8 @@ if __name__ == "__main__":
     data_RG8 = np.load(file, mmap_mode="r")
     n_p, n_T, n_U = data_RG8.shape
 
-    p_idx = 2
-    T_idx = 150
+    p_idx = 0
+    T_idx = 10
 
     data_info = data_RG8[p_idx, T_idx]
 
@@ -34,58 +73,50 @@ if __name__ == "__main__":
     T = data_info[1]
     p_all = data_info[2:]
     model.update_p_all(T, p_all)
+    J_exact = get_RG8_a_jacobian(model)
 
+
+    model.update_p_all(T, p_all)
     rho = model.rho_mix
     T = model.T
     eta_all = model.eta_all
 
-    
+    n_Y = 10
+    U0 = np.zeros(n_Y)
+    U0[:8] = eta_all
+    U0[-2] = rho
+    U0[-1] = T
 
-    print(rho, T)
+    res_0 = res_a(U0, model)
 
+    dx = 1e-6
+    J_FDM = np.zeros((n_Y, n_Y))
+    for u_idx in range(n_Y):
+        U1 = np.array(U0)
+        U2 = np.array(U0)
+        dy = U0[u_idx]*dx
 
-
-'''
-    dx_array = np.array([1e-5, 1e-4, 1e-3, 1e-2, 1e-1])
-    #dp_array = np.array([1e-7])
-    err_list = []
-
-    for dp in dp_array:
-        model.init_composition([0.79, 0.21, 0.02, 0.05, 0.07, 0.04, 0.01, 1e-3])
-
-        p_0 = p_mix*model.x0_all
-
-        p_0 = np.array([7.92707301e+01, 2.03054798e+01, 8.11870550e-01, 2.73816643e-06,
-            9.36916799e-01, 2.79899932e-22, 2.34349282e-14, 2.34349285e-14])
-
-
-        K_p_all = model.compute_K_p(T)
-        ratio_NO = model.ratio_NO
+        U1[u_idx] += dy
+        res_1 = res_a(U1, model)
         
-        res_0 = np.array(res_RG8(p_0, p_mix, K_p_all, ratio_NO))
-        res_prime_0 = res_RG8_prime(p_0, p_mix, K_p_all, ratio_NO)
+        U2[u_idx] -= dy
+        res_2 = res_a(U2, model)
 
-        res_fdm = np.zeros((n_s, n_s))
-        for idx in range(n_s):
-            p = np.array(p_0)
-
-            p[idx] += dp
-
-            res_dp = np.array(res_RG8(p, p_mix, K_p_all, ratio_NO))
+        dres_dx = (res_1 - res_2)/(2*dy)
 
 
-            res_fdm[:, idx] = (res_dp - res_0)/dp
+        J_FDM[:, u_idx] = dres_dx
 
-        dev = res_fdm - res_prime_0
+    u_idx = 9
 
-        error = np.max(np.abs(dev))
+    dev = J_FDM[u_idx] - J_exact[u_idx]
+    y = J_exact[u_idx]
+    ref = np.where(np.abs(y) > 1e-20, np.abs(y), 1e-20)
+    err_ref = dev/ref
 
-        err_list += [error]
-
-
-    plt.loglog(dp_array, err_list, "-ob")
-    plt.grid()
-    plt.ylabel(r"$L^{\infty}$ error", fontsize=14)
-    plt.xlabel(r"$\Delta p$", fontsize=14)
-    plt.show()
-'''
+    print(err_ref)
+    print()
+    #print(np.max(np.abs(err_ref)))
+    print(J_exact[u_idx])
+    print(J_FDM[u_idx])
+    print()

libs/real-gas/Mixture.py
@@ -422,22 +422,27 @@ def res_RG8_prime(p, p_mix, K_p_all, ratio_NO):
 
     return res_prime
 
-def get_RG8_a_jacobian(mixture, dT=1e-3):
+def get_RG8_a_jacobian(mixture, dx=1e-8):
+
+    p_all_0 = mixture.p_all
+
     T0 = mixture.T
+    dT = T0*dx
     T1 = T0 + dT
+    T2 = T0 - dT
+
 
     # FDM approach
     K_p_all_1 = mixture.compute_K_p(T1)
-    mixture.compute_all()
-    s_mix_1 = mixture.s_mix
-
+    K_p_all_2 = mixture.compute_K_p(T2)
     K_p_all_0 = mixture.compute_K_p(T0)
+
     mixture.compute_all()
     s_mix_0 = mixture.s_mix
     p_all = mixture.p_all
 
-    dK_pdT = (K_p_all_1 - K_p_all_0)/dT
-    dsdT = (s_mix_1 - s_mix_0)/dT
+    dK_pdT = (K_p_all_1 - K_p_all_2)/(2*dT)
+    #dsdT = (s_mix_1 - s_mix_2)/(2*dT)
 
 
     eta_all = mixture.eta_all
@@ -447,36 +452,37 @@ def get_RG8_a_jacobian(mixture, dT=1e-3):
 
     rhoRT = rho*R*T0
 
-    Jacobian = np.zeros((10, 10))
+    n_Y = n_s + 2
+    Jacobian = np.zeros((n_Y, n_Y))
 
     # reaction equlibrium
     Jacobian[0, 0] = -eta_all[3]**2/eta_all[0]**2
     Jacobian[0, 3] = 2*eta_all[3]/eta_all[0]
     Jacobian[0, -2] = K_p_all_0[0]/(rho*rhoRT)
-    Jacobian[0, -1] = (1 - T0*dK_pdT[0])/(rhoRT*T0)
+    Jacobian[0, -1] = (K_p_all_0[0] - T0*dK_pdT[0])/(rhoRT*T0)
 
     Jacobian[1, 1] = -eta_all[4]**2/eta_all[1]**2
     Jacobian[1, 4] = 2*eta_all[4]/eta_all[1]
     Jacobian[1, -2] = K_p_all_0[1]/(rho*rhoRT)
-    Jacobian[1, -1] = (1 - T0*dK_pdT[1])/(rhoRT*T0)
+    Jacobian[1, -1] = (K_p_all_0[1] - T0*dK_pdT[1])/(rhoRT*T0)
 
     Jacobian[2, 2] = -eta_all[3]*eta_all[4]/eta_all[2]**2
     Jacobian[2, 3] = eta_all[4]/eta_all[2]
     Jacobian[2, 4] = eta_all[3]/eta_all[2]
     Jacobian[2, -2] = K_p_all_0[2]/(rho*rhoRT)
-    Jacobian[2, -1] = (1 - T0*dK_pdT[2])/(rhoRT*T0)
+    Jacobian[2, -1] = (K_p_all_0[2] - T0*dK_pdT[2])/(rhoRT*T0)
 
     Jacobian[3, 3] = -eta_all[5]*eta_all[7]/eta_all[3]**2
     Jacobian[3, 5] = eta_all[7]/eta_all[3]
     Jacobian[3, 7] = eta_all[5]/eta_all[3]
     Jacobian[3, -2] = K_p_all_0[3]/(rho*rhoRT)
-    Jacobian[3, -1] = (1 - T0*dK_pdT[3])/(rhoRT*T0)
+    Jacobian[3, -1] = (K_p_all_0[3] - T0*dK_pdT[3])/(rhoRT*T0)
 
     Jacobian[4, 4] = -eta_all[6]*eta_all[7]/eta_all[4]**2
     Jacobian[4, 6] = eta_all[7]/eta_all[4]
     Jacobian[4, 7] = eta_all[6]/eta_all[4]
     Jacobian[4, -2] = K_p_all_0[4]/(rho*rhoRT)
-    Jacobian[4, -1] = (1 - T0*dK_pdT[4])/(rhoRT*T0)
+    Jacobian[4, -1] = (K_p_all_0[4] - T0*dK_pdT[4])/(rhoRT*T0)
 
     # conservations
     A = 2*eta_all[0] + eta_all[2] + eta_all[3] + eta_all[5]
@@ -493,20 +499,72 @@ def get_RG8_a_jacobian(mixture, dT=1e-3):
     Jacobian[6, 6] = 1
     Jacobian[6, 7] = -1
 
+    U0 = np.zeros(n_Y)
+    U0[:8] = eta_all
+    U0[-2] = rho
+    U0[-1] = T0
+
+    Jacobian[9] = get_ds_mix_dU(eta_all, rho, T0, mixture, dx)
+
     for idx in range(n_s):
         species = mixture.species_list[idx]
 
         Jacobian[7, idx] = species.M_hat
         Jacobian[8, idx] = rhoRT
-        Jacobian[9, idx] = species.M_hat*species.s_total
+        Jacobian[9, idx] = species.M_hat*species.s_total - R
 
     Jacobian[8, -2] = np.sum(eta_all)*R*T0
     Jacobian[8, -1] = np.sum(eta_all)*rho*R
 
     Jacobian[9, -2] = np.sum(-eta_all**2*R**2*T0/p_all)
-    Jacobian[9, -1] = dsdT
-
+    #Jacobian[9, -1] = dsdT
+    mixture.set_T(T0)
     return Jacobian
+
+def get_ds_mix_dU(eta_all, rho, T, mixture, dx, n_Y=10):
+    U0 = np.zeros(n_Y)
+    U0[:8] = eta_all
+    U0[-2] = rho
+    U0[-1] = T
+
+    dsdU = np.zeros(n_Y)
+    for u_idx in range(n_Y):
+        U1 = np.array(U0)
+        U2 = np.array(U0)
+        dy = U0[u_idx]*dx
+
+        U1[u_idx] += dy
+        s_1 = get_s_mix(U1, mixture)
+        
+        U2[u_idx] -= dy
+        s_2 = get_s_mix(U2, mixture)
+
+        dsdU[u_idx] = (s_1 - s_2)/(2*dy)
+
+    return dsdU 
+
+def get_s_mix(U, mixture):
+    eta_all = U[:8]
+    rho = U[-2]
+    T = U[-1]
+
+    n_s = mixture.n_species
+    R_hat = mixture.R_hat
+
+    s_mix = 0 
+
+    mixture.set_T(T)
+    for s_idx in range(n_s):
+        species = mixture.species_list[s_idx]
+        p_s = eta_all[s_idx]*rho*R_hat*T
+        species.compute_entropy(p_s)
+        s_total = species.s_total
+
+        cs = eta_all[s_idx]*species.M_hat*s_total
+
+        s_mix += cs
+
+    return s_mix
 
 if __name__ == "__main__":
     pass